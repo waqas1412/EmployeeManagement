1f7bc39bd304876b954bf900e068606aac0b55d2
Waqas Khan <muhammad.waqas1412@hotmail.com>
2021-06-16 13:42:10 +0500

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        // Other Code
+
+       
+    }
+}
+
